e95ad47ec7723aa4a73d2448a368a0555f9653ae
Jürgen Roos <44707918+darthrevanyunka@users.noreply.github.com>
2026-04-17 21:05:43 +0200

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: ".",
-    emptyOutDir: false,
-    rollupOptions: {
-      output: {
-        entryFileNames: "[name]-[hash].js",
-        chunkFileNames: "chunk-[hash].js",
-        assetFileNames: "[name]-[hash][extname]",
-      },
-    },
-  },
 });
